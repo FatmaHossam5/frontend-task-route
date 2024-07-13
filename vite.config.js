d54661ee1987604ffe2a8c0a158b00546c3af980
FatmaHossam5 <eng.fatma.fateh@gmail.com>
2024-07-13 19:26:33 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: /frontend-task-route/,
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: './src/main.jsx'
-      }
-    }
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
   },
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5001'
-    }
-  }
+  plugins: [react()],
+  base: /frontend-task-route/
 });
